a33b7e58404d1dc9c82fb98543afd32a4143368f
amongtheflowers <919056595@qq.com>
2020-05-10 16:07:32 +0800

    11

--- gadget/plugins/other/noop_chat.py
+++ gadget/plugins/other/noop_chat.py
@@ -3,19 +3,20 @@
 
 
 from . import other
-from gadget.untils.chat import Chat
+from gadget.untils.chat import ChatNvPu
 import nonebot as rcnb
 
 
 @other.command('chat')
 async def _(session: rcnb.CommandSession):
     msg = session.get_optional('msg')
-    report = await Chat.request(msg)
+    report = await ChatNvPu.request(msg)
     await session.finish(report)
     return
 
 
 @rcnb.on_natural_language()
 async def _(session: rcnb.NLPSession):
-    if session.ctx
-    #return rcnb.IntentCommand(60.0, ('other', 'chat'), {'msg': session.msg_text})
+    if session.event.user_id != 919056595:
+        return
+    return rcnb.IntentCommand(60.0, ('other', 'chat'), {'msg': session.msg_text})
